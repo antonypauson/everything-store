58b838a6603b2aaea32b476854af364be9472bb8
antonypauson <13antonysun@gmail.com>
2025-10-01 17:35:23 +0530

    test: product get all

--- backend/tests/e2e/product.e2e.test.ts
+++ backend/tests/e2e/product.e2e.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { AppDataSource } from "../../src/data-source";
 import app from "../../src/app";
+import { Product } from "../../src/entity/Product";
 
 describe("Product E2E tests", () => {
   let server: any;
@@ -18,6 +19,12 @@ describe("Product E2E tests", () => {
     server.close();
   });
 
+  //make sure we clear everything before
+  // each test
+  beforeEach(async () => {
+    await AppDataSource.synchronize(true);
+  });
+
   it("should return 200 for the root path", async () => {
     const res = await request(server).get("/");
     expect(res.statusCode).toEqual(200);
@@ -25,20 +32,58 @@ describe("Product E2E tests", () => {
 
   it("should create product for POST /api/products", async () => {
     const newProduct = {
-        name: 'testing product', 
-        description: 'testing product description',
-        price: 99999,
-    }
+      name: "testing product",
+      description: "testing product description",
+      price: 99999,
+    };
     //request
-    const res = await request(server).post('/api/products').send(newProduct).expect(200); 
+    const res = await request(server)
+      .post("/api/products")
+      .send(newProduct)
+      .expect(200);
 
     //assertions
-    expect(res.body).toBeDefined(); 
+    expect(res.body).toBeDefined();
     expect(res.body.id).toBeDefined(); //id check
-    expect(res.body.name).toEqual(newProduct.name); 
-    expect(res.body.price).toEqual(newProduct.price); 
-  })
+    expect(res.body.name).toEqual(newProduct.name);
+    expect(res.body.price).toEqual(newProduct.price);
+  });
 
-  
+  it("should return empty array if no products", async () => {
+    const res = await request(server).get("/api/products").expect(200);
+    expect(res.body).toBeInstanceOf(Array);
+    expect(res.body.length).toEqual(0);
+  });
 
+  it("should return all products", async () => {
+    const placeholderProducts = [
+      { name: "test pro 1", price: 100 },
+      { name: "test pro 2", price: 100 },
+      { name: "test pro 3", price: 100 },
+    ];
+    await Promise.all(
+      placeholderProducts
+        .map((product) => request(server).post("/api/products")
+        .send(product)
+        .expect(200))
+    );
+
+    //get
+    const res = await request(server).get("/api/products").expect(200); 
+
+    expect(res.body).toBeInstanceOf(Array); 
+    expect(res.body.length).toEqual(placeholderProducts.length);
+    expect(res.body).toEqual(
+        expect.arrayContaining(
+            placeholderProducts.map((product) => 
+                expect.objectContaining({
+                    id: expect.any(Number), 
+                    name: product.name,
+                    price: product.price, 
+                    description: expect.any(String),
+                    stock: expect.any(Number)
+                }))
+        )
+    ) 
+  });
 });
