053a6ab88908b331302f4d444ffbda63e000a7fe
antonypauson <13antonysun@gmail.com>
2025-10-01 15:37:32 +0530

    init: supertest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.test.ts"],
+  setupFiles: ["<rootDir>/tests/setup.ts"],
   moduleFileExtensions: ["ts", "js", "json", "node"],
   roots: ["<rootDir>/tests", "<rootDir>/src"],
   moduleNameMapper: {
