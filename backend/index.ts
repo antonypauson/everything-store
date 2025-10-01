dc38a47fe56dab07f3a4493bb4c5fa06a3e4705a
antonypauson <13antonysun@gmail.com>
2025-10-01 15:42:07 +0530

    refactor: re-config app

--- backend/index.ts
+++ backend/index.ts
@@ -3,18 +3,22 @@ import app from "./src/app";
 import { initializeTypeORM } from "./src/data-source";
 const PORT = process.env.PORT || 3000;
 
-//init db first
-(async () => {
-  try {
-    await initializeTypeORM();
-    console.log("Database connection success");
-  } catch (error) {
-    console.log("Failed to intialize db", error);
-    process.exit(1);
-  }
-})();
+//don't run if imported
+if (require.main === module) {
+  //init db first
+  (async () => {
+    try {
+      await initializeTypeORM();
+      console.log("Database connection success");
+    } catch (error) {
+      console.log("Failed to intialize db", error);
+      process.exit(1);
+    }
+  })();
 
-//only start server after db
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  //only start server after db
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
